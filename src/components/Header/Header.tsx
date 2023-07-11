33218316d368ff0c69b3cb651b007e0acec7f8e3
sgkochar <sgkochar@gmail.com>
2023-07-11 14:20:02 +0300

    add Login Window

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 815,
+  bgcolor: '#191E2E',
+  border: '2px solid #000',
+  borderRadius: '8px',
+  boxShadow: 24,
+  p: 4,
+};
+
+
 const Header = () => {
+  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);  
+ 
   return (
    <header className="header d-f jc-sb align-center">
     <div>
@@ -27,7 +51,26 @@ const Header = () => {
     </nav>
     <div className='header-actions d-f'>
        <button><img src="images/search.png" alt="search" /></button>
-       <button>Увійти</button>
+       <button onClick={handleOpen}>Увійти</button>
+       <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <button className='modal-action' onClick={handleClose}><img src="images/close.png" alt="close" /></button>  
+          <h2 className="modal-title">Увійти</h2>
+          <form action="" className="modal-fm">
+            <input type="text" name='login' placeholder='Логін, пошта або телефон' />
+            <input type="password" name="password" placeholder='Ваш пароль' />
+            <button className="modal-fm-enter">Увійти</button>
+            <button className="modal-fm-sign">Зареєструватись</button>
+            <a href="" className="modal-fm-restore">Відновити пароль</a>
+            <div className="modal-fm-socials"><img src="images/socials.png" alt="socials"/></div>
+          </form>
+        </Box>
+      </Modal>
     </div>
         
    </header>
